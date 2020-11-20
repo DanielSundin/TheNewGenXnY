04583a49c4d7408d2c6580f93b91ea6f7a1a7a0b
Jonathan Markert <jonathan@markert.se>
2020-11-20 15:38:57 +0100

    Statistic class added

---
+++ SSCEUPClassLibrary/Statistic.cs
@@ -0,0 +1,14 @@
+namespace SSCEUPClassLibrary
+{
+    class Statistic
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public bool IsYesNoQuestion { get; set; }
+        public int AVG { get; set; }
+        public int MAX { get; set; }
+        public int MIN { get; set; }
+        public int YESPROCENT { get; set; }
+        
+    }
+}
